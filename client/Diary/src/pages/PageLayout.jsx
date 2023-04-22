8ac0a72af120a68e8f13099309b98408371cd774
Rosita E <RositaEmakpo@Gmail.com>
2023-04-22 15:18:28 -0700

    Corrects Layout for text editor

--- client/Diary/src/pages/PageLayout.jsx
+++ client/Diary/src/pages/PageLayout.jsx
@@ -15,10 +15,12 @@ import 'react-quill/dist/quill.snow.css';
 const PageLayout = () => {
 
   return (
-    <div class="parent">
-      <div class="div1"> 1 </div>
-      <div class="div2"> 2 </div>
-      <ReactQuill className="div3" contentEditable="true" />
+    <div className="parent">
+      <div className="div1"> 1 </div>
+      <div className="div2"> 2 </div>
+      <div className="div3">
+        <ReactQuill className="quill" contentEditable="true" />
+      </div>
     </div>
   )
 }
