beda94436367023566ca70b52d4334afe2313179
Rosita E <RositaEmakpo@Gmail.com>
2023-04-22 14:07:39 -0700

    Adds package

--- client/Diary/src/pages/PageLayout.jsx
+++ client/Diary/src/pages/PageLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/pageLayout.css';
 import {
   IconTrash,
@@ -7,29 +7,18 @@ import {
   IconLock,
   IconLockOpen
 } from "@tabler/icons-react";
-import { Button } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
-const { Search } = Input;
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+
 
 const PageLayout = () => {
-  const onSearch = (value) => console.log(value);
 
   return (
-    <div className="parent">
-      <div className="div1">
-        <div className="serchBox">
-          <Input style={{ width: "55%" }} placeholder='Search' prefix={<SearchOutlined style={{ color: "pink" }} />} />
-        </div>
-      </div>
-      <div className="div2"> 2 </div>
-      <div className="div3"> 3 </div>
-      <div className="div4"> 4 </div>
-      <div className="div5"> 5 </div>
-      <textarea className="div6" contentEditable="true" />
-      <div className="div7">
-        <Button className="btn" type="primary">Save</Button>
-      </div>
+    <div class="parent">
+      <div class="div1"> 1 </div>
+      <div class="div2"> 2 </div>
+      <ReactQuill className="div3" contentEditable="true" />
     </div>
   )
 }
